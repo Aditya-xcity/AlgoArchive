8727363da71ca1ae7689407bc26290e864433e68
Aditya Bhardwaj <adityabhardwajpcm@gmail.com>
2026-05-30 20:33:45 +0530

    Pattern/Pattern4.cpp

---
+++ Pattern/Pattern4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+void pat(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<i;
+        }
+        cout<<"\n";
+
+    }
+}
+
+int main()
+{
+int n;
+cin>>n;
+
+
+pat(n);
+
+
+    return 0;
+}
