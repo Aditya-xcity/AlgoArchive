c699d61b1f4f77b6d15b22b15916d4f401d18e17
Aditya Bhardwaj <adityabhardwajpcm@gmail.com>
2026-05-31 21:11:17 +0530

    Pattern7.cpp

---
+++ Pattern/Pattern7.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+
+void pat(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<"-";
+            
+        }
+        for(int j=0;j<2*i+1;j++)
+        {
+            cout<<"*";
+            
+        }
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<"-";
+            
+        }
+        cout<<"\n";
+        
+
+    }
+}
+
+int main()
+{
+int n;
+cin>>n;
+
+
+pat(n);
+
+
+    return 0;
+}
