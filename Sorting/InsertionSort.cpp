9214ff5743ce6183fc33adf52b52a3fcfd83d606
Aditya Bhardwaj <adityabhardwajpcm@gmail.com>
2026-06-13 23:22:19 +0530

    code fix

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,68 +1,51 @@
+// Aim: To sort an array using Insertion Sort algorithm.
+
+/*
+Name - ADITYA BHARDWAJ
+Section - D2
+Roll No - 07
+Course – B TECH
+Branch – CSE
+*/
+
 #include<iostream>
 using namespace std;
 
-
-void insertionSort(int A[],int n)
+void insertionSort(int A[], int n)
 {
+    int key, j;
 
-    //12,54,65,7,23,9
-    int key,j;
+    for(int i = 1; i < n; i++)
+    {
+        key = A[i];
+        j = i - 1;
 
-//loop for passes
-for(int i=1;i<n-1;i++)
-{
+        while(j >= 0 && A[j] > key)
+        {
+            A[j + 1] = A[j];
+            j--;
+        }
 
- key = A[i];
- j = i-1;
-
- //loop for each pass
-
-
-
+        A[j + 1] = key;
+    }
 }
 
-
-
-
-
-}
-
-
-
-void printArray(int A[],int n)
+void printArray(int A[], int n)
 {
-for(int i=0;i<n;i++)
-{
-    cout<<A[i]<<" ";
+    for(int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
 }
-cout<<endl;
-}
-
 
 int main()
 {
-
-
-    // 4 5 6 7 3 2
-
-
-    int A[] = {12,54,65,7,23,9};
+    int A[] = {12, 54, 65, 7, 23, 9};
     int n = 6;
-    insertionSort(A,n);
-    printArray(A,n);
 
-
-
-
-
-
-
-
-
-
-
-
-
+    insertionSort(A, n);
+    printArray(A, n);
 
     return 0;
 }
